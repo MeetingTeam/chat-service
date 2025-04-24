a9ffb9d0880c4fecab68074bb877665089fcb3f6
HungTran170904 <tienhung17092004@gmail.com>
2025-04-24 23:24:36 +0700

    docs: comment some things

--- src/main/java/meetingteam/chatservice/ChatServiceApplication.java
+++ src/main/java/meetingteam/chatservice/ChatServiceApplication.java
@@ -9,6 +9,7 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 @EnableConfigurationProperties(ServiceUrlConfig.class)
 public class ChatServiceApplication {
 
+    // Comment some thing
     public static void main(String[] args) {
         SpringApplication.run(ChatServiceApplication.class, args);
     }
